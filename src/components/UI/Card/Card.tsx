da6ef4394d8300c0178edd65db553fa31ddb24f0
R9 Mobile dev <asamir@road9media.com>
2021-03-13 00:11:52 +0200

    Enhance Card Design

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
-import styles from './style';
+import React from "react";
+import { View, TouchableOpacity } from "react-native";
+import styles from "./style";
 
 export const Card = ({
-    disabled,
-    children,
-    onPress,
+  disabled,
+  children,
+  onPress,
+  style,
 }: {
-    disabled?: boolean | undefined;
-    children?: any;
-    onPress?: Function | undefined;
+  disabled?: boolean | undefined;
+  children?: any;
+  onPress?: Function | undefined;
+  style?: object;
 }) => {
-    return (
-        <TouchableOpacity
-            disabled={disabled}
-            style={styles.cardCont}
-            onPress={onPress}
-            activeOpacity={0.7}>
-            <View style={styles.cardInner}>{children}</View>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity
+      disabled={disabled}
+      style={[styles.cardCont, style]}
+      onPress={onPress}
+      activeOpacity={0.7}
+    >
+      <View style={styles.cardInner}>{children}</View>
+    </TouchableOpacity>
+  );
 };
