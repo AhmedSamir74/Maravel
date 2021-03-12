49428fec24bed371538ee4245e40d77acfcf4e41
R9 Mobile dev <asamir@road9media.com>
2021-03-13 00:32:25 +0200

    add arabic localiztion

--- src/localization/ar.ts
+++ src/localization/ar.ts
@@ -1,6 +1,24 @@
 const ar = {
-    //General
-    getStarted: 'skip',
+  //General
+  getStarted: "skip",
+  pickPoster: "Pick a poster",
+  title: "Title",
+  overview: "Overview",
+  date: "Date",
+  submit: "Submit",
+  myMovies: "My Movies",
+  allMovies: "All Movies",
+  addMovie: "Add Movie",
+  confirm: "Confirm",
+  cancel: "Cancel",
+  search: "Search for a character...",
+  description: "Description",
+  comics: "Comics",
+  events: "Events",
+  series: "Series",
+  stories: "Stories",
+  emptyList: "List is empty",
+  noResults: "No results found try another name!",
 };
 
 export default ar;
